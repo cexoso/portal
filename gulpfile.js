9ad231e86acf4a0495bc78488096cc24f66be59f
cexoso <285516193@qq.com>
2016-02-18 16:12:34 +0800

    nd

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,19 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
    jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create();
 gulp.task('server', function() {
-    
-  connect.server({
-    root: ['app'],
-    livereload: true,
-    port:80
+  browserSync.init({
+    server:{
+      baseDir: ['app','bower_components']
+    }
   });
-  var rel=['app/**/*.html','app/scripts/**/*.js','app/styles/**/*.css'];
-  gulp.watch(rel,function(){
-    gulp.src('./app/**/*.html')
-    .pipe(connect.reload());
+  gulp.watch("app/styles/*.css",function(d){
+    console.log(d);
+    return gulp.src(d.path)
+      .pipe(browserSync.stream());
   });
 
-});
-
-gulp.task('lint', function() {
-   var js=['app/scripts/**/*.js'];
-    gulp.watch(js,function(){
-        gulp.src(js)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-    });
 });
 
 gulp.task('default', ['server']);
-
-
-
